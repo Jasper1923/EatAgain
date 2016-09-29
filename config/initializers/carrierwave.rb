11bab028714299330c8423a36dc172e23c683b88
shooter <shooter.hero@gmail.com>
2016-09-29 18:03:15 +0800

    修改本地配置

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider              => 'AWS',                        # required
-    :s3_access_key_id      => ENV["S3_ACCESS_KEY"],         # required
-    :s3_secret_access_key  => ENV["s3_secret_KEY"],         # required
-  }
-  config.fog_directory  = ENV["S3_Bucket"]              # required
+  # config.fog_credentials = {
+  #   :provider              => 'AWS',                        # required
+  #   :s3_access_key_id      => ENV["S3_ACCESS_KEY"],         # required
+  #   :s3_secret_access_key  => ENV["s3_secret_KEY"],         # required
+  # }
+  # config.fog_directory  = ENV["S3_Bucket"]              # required
 end
