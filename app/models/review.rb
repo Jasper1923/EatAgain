4248bfd39a2e646427ced15ce0be69fb4dad842c
jasper <jasper1923@hotmail.com>
2016-10-07 21:54:32 +0800

    change about me

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,7 +7,7 @@ class Review < ActiveRecord::Base
       only_integer: true,
       greater_than_or_equal_to: 1,
       less_than_or_equal_to: 5,
-      message: "Can Only Be a Number Between 1 to 5"
+      message: "Can Only be a Number between 1 to 5"
       }
 
 end
